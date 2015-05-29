f4335753de1100dbdafae6d0d853ba0f22afe71d
lokson <kapustka.maciek@gmail.com>
2015-05-29 13:05:36 +0200

    template fix

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -1,21 +1,21 @@
 # N1
-class N1sController < ApplicationController
-  @model = N1
-  include ElementsController
-
-  def create
-    super
-  end
-
-  def update
-    super
-  end
-
-  def destroy
-    super
-  end
-
-  def element_params
-    params.require(:element).permit :name
-  end
-end
+# class N1sController < ApplicationController
+#   @model = N1
+#   include ElementsController
+#
+#   def create
+#     super
+#   end
+#
+#   def update
+#     super
+#   end
+#
+#   def destroy
+#     super
+#   end
+#
+#   def element_params
+#     params.require(:element).permit :name
+#   end
+# end

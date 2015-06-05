0108d6a910596c4fe8108ab640726daec854689d
lokson <kapustka.maciek@gmail.com>
2015-06-05 15:55:50 +0200

    labels, groups, on_click

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
   include ElementsController
 
   def element_params
-    params.require(:element).permit :name, :password, :role_id
+    params.require(:element).permit :name, :password, :role_id, :last_login_at, :logins_count
   end
 end
